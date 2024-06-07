0e9ac611002eb96a707a8f758593197a8dbf6546
Daniel Elias Jimenez Rojas <djimenez.dev@outlook.com>
2024-06-07 13:25:56 -0400

    Complete follow toggle endpoint

---
+++ Api/Controllers/FollowController.cs
@@ -0,0 +1,14 @@
+﻿using Application.Followers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+public class FollowController : BaseApiController
+{
+    [HttpPost("{username}")]
+    public async Task<IActionResult> Follow(string username)
+    {
+        return HandleResult(await Sender.Send(new FollowToggle.Command(username)));
+    }
+
+}
